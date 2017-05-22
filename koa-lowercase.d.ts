df1d435ad8690e738833b5030e8584733fb8347b
zacanger <zac@zacanger.com>
2017-05-22 15:34:37 -0600

    Fix typings.

--- koa-lowercase.d.ts
+++ koa-lowercase.d.ts
@@ -1,4 +1,4 @@
 import { Middleware } from 'koa'
-declare function lowercase (ctx, next): Middleware
+declare function lowercase (ctx: any, next: () => Promise<any>): Middleware
 declare namespace lowercase {}
 export = lowercase
